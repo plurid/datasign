b91172d8bbc55086074a15ced3ae0a9d00655efe
Jan Cavel <caveljan@gmail.com>
2020-03-21 17:27:27 +0200

    datasign > dev: cli

---
+++ packages/datasign/source/data/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface CompileData {
+    files: string[];
+    target: string[];
+    output: string;
+    resolve: string;
+}
--- packages/datasign/source/index.ts
+++ packages/datasign/source/index.ts
@@ -2,6 +2,18 @@ import program, {
     CommanderStatic,
 } from 'commander';
 
+import {
+    compileCommand,
+} from './commands';
+
+import {
+    CompileData,
+} from './data/interfaces';
+
+import {
+    separateList,
+} from './services/utilities';
+
 
 
 async function main(
@@ -9,9 +21,43 @@ async function main(
 ) {
     program
         .name('datasign')
-        .usage('<command>')
-        .version('0.1.0', '-v, --version');
+        .usage('<files>')
+        .version('0.1.0', '-v, --version')
+        .action(async (_, files) => {
+            if (!files) {
+                program.outputHelp();
+                return;
+            }
 
+            const target = separateList(program.target);
+            const {
+                output,
+                resolve,
+            } = program;
+
+            const data: CompileData = {
+                files,
+                target,
+                output,
+                resolve,
+            };
+            await compileCommand(data);
+        });
+
+    program
+        .option(
+            '-t, --target <type>',
+            'compilation targets: typescript, graphql, protobuf',
+            'typescript,graphql,protobuf'
+        ).option(
+            '-o, --output <path>',
+            'output path',
+            '.',
+        ).option(
+            '-r, --resolve <type>',
+            'resolve the output path relative to "file" directory, "process" directory, or "flatten" into the output path',
+            'file',
+        );
 
     program.parseAsync(process.argv);
 }
