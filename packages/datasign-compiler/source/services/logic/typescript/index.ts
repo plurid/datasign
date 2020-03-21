7c3f6835fe53728c0d111f4147ee7a73f2873d72
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-21 23:33:23 +0200

    datasign-compiler > dev: logic > typescript

--- packages/datasign-compiler/source/services/logic/typescript/index.ts
+++ packages/datasign-compiler/source/services/logic/typescript/index.ts
@@ -1,24 +1,41 @@
 import {
     DatasignEntity,
+    DatasignEntityData,
 } from '../../../data/interfaces';
 
 
+
+const generateTypescriptFields = (
+    data: DatasignEntityData[],
+) => {
+    const fields: string[] = [];
+
+    for (const field of data) {
+        const fieldText = `${field.name}${field.required ? '?' : ''}: ${field.type};`;
+        fields.push(fieldText);
+    }
+
+    return fields;
+}
+
+
+const generateTypescriptEntity = (
+    entity: DatasignEntity,
+) => {
+    const fields = generateTypescriptFields(entity.data);
+
+    const entityText = `
+export interface ${entity.name} {
+${fields.map(field => '    ' + field)}
+}
+`;
+    return entityText;
+}
 
 const generateTypescript = (
     parsed: DatasignEntity[],
 ) => {
     let typescriptText = '';
-
-    const generateTypescriptEntity = (
-        entity: DatasignEntity,
-    ) => {
-        const entityText = `
-export interface ${entity.name} {
-id: string;
-}
-        `;
-        return entityText;
-    }
 
     for (const entity of parsed) {
         const entityText = generateTypescriptEntity(entity);
