25ec2ed9f686a211ba3161fa5da876b06b39c43e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-21 20:45:50 +0200

    datasign > dev: cli

--- packages/datasign/source/index.ts
+++ packages/datasign/source/index.ts
@@ -1,75 +1,9 @@
-import program, {
-    CommanderStatic,
-} from 'commander';
-
 import DatasignCompiler, {
     DatasignLoader,
 } from '@plurid/datasign-compiler';
 
-import {
-    compileCommand,
-} from './commands';
+import cli from './cli';
 
-import {
-    CompileData,
-} from './data/interfaces';
-
-import {
-    separateList,
-} from './services/utilities';
-
-
-
-const main = async (
-    program: CommanderStatic,
-) => {
-    program
-        .name('datasign')
-        .usage('<files>')
-        .version('0.1.0', '-v, --version')
-        .action(async (_, files) => {
-            if (!files) {
-                program.outputHelp();
-                return;
-            }
-
-            const {
-                target,
-                output,
-                resolve,
-            } = program;
-            const targets = separateList(target);
-
-            const data: CompileData = {
-                files,
-                targets,
-                output,
-                resolve,
-            };
-            await compileCommand(data);
-        });
-
-    program
-        .option(
-            '-t, --target <type>',
-            'compilation targets: typescript, graphql, protobuf',
-            'typescript,graphql,protobuf',
-        ).option(
-            '-o, --output <path>',
-            'output path',
-            '.',
-        ).option(
-            '-r, --resolve <type>',
-            'resolve the output path relative to the "file" directory, "process" directory, or "flatten" into the output path',
-            'file',
-        );
-
-    program.parseAsync(process.argv);
-}
-
-const cli = () => {
-    main(program);
-}
 
 
 export {
