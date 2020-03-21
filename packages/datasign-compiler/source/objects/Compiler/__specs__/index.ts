e252ed395279af32d882dfec4a99a2ad19bb9672
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-21 22:34:43 +0200

    datasign-compiler > dev: Compiler, logic > source

--- packages/datasign-compiler/source/objects/Compiler/__specs__/index.ts
+++ packages/datasign-compiler/source/objects/Compiler/__specs__/index.ts
@@ -8,9 +8,33 @@ import {
 describe('DatasignCompiler', () => {
     it('basic', () => {
         const data: DatasignCompilerData = {
+//             source: `
+// data Item {
+//     id: string;
+// }
+//             `,
             source: `
-data Item {
+@entityID: TextEntity // assigns an ID to the type itself
+data Text {
+    // type the 'id' field to 'ID' in GraphQL, and 'string' for TypeScript/Protocol Buffers/gRPC
+    @graphql: ID
     id: string;
+
+    name: string;
+    value: string;
+    @graphql: Int
+    characters: number;
+    public: boolean;
+
+    @graphql: Date
+    @protobuf: number
+    generatedAt: Date;
+    generatedBy: User;
+}
+
+data User {
+    id: string;
+    name: string;
 }
             `,
             targets: [
@@ -36,6 +60,8 @@ message Item {
     required string id = 1;
 }
         `;
+
+        // console.log(result);
 
         // expect(result.typescript).toBe('');
         expect('').toBe('');
