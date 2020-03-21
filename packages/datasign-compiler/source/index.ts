ab62956a543066a99ba6e18cd4dc8287c57e626b
Jan Cavel <caveljan@gmail.com>
2020-03-21 17:28:46 +0200

    datasign-compiler > dev: Loader

--- packages/datasign-compiler/source/index.ts
+++ packages/datasign-compiler/source/index.ts
@@ -1,8 +1,12 @@
 import DatasignCompiler from './objects/Compiler';
+import DatasignLoader from './objects/Loader';
 
 
 
 export * from './data/interfaces';
 
+export {
+    DatasignLoader,
+}
 
 export default DatasignCompiler;
