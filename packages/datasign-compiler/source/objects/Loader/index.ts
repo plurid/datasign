5cc8069fd5fa91b3a76c9b81b79c251ee38801f9
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-21 22:01:47 +0200

    datasign-compiler > dev: Loader

--- packages/datasign-compiler/source/objects/Loader/index.ts
+++ packages/datasign-compiler/source/objects/Loader/index.ts
@@ -1,11 +1,70 @@
+import path from 'path';
+import {
+    promises as fs,
+} from 'fs';
+
+import {
+    targets,
+} from '../../data/constants';
+
+import {
+    Target,
+    DatasignEntity,
+} from '../../data/interfaces';
+
+import {
+    parseSource,
+
+    generateGraphql,
+    generateProtobuf,
+    generateTypescript,
+} from '../../logic';
+
+
+
 class Loader {
-    constructor() {
+    private source: string;
+    private parsedSource: DatasignEntity[] = [];
 
+    constructor(
+        source: string,
+    ) {
+        this.source = source;
     }
 
-    load() {
+    async load(
+        target: Target,
+    ) {
+        if (!isDatasignText(this.source) && this.parsedSource === []) {
+            try {
+                const filepath = path.join(process.cwd(), this.source);
+                const content = await fs.readFile(filepath, 'utf-8');
+                this.source = content;
+                this.parsedSource = parseSource(this.source);
+            } catch (error) {
+            }
+        }
 
+        if (this.parsedSource === []) {
+            this.parsedSource = parseSource(this.source);
+        }
+
+        switch (target) {
+            case targets.graphql:
+                return generateGraphql(this.parsedSource);
+            case targets.protobuf:
+                return generateProtobuf(this.parsedSource);
+            case targets.typescript:
+                return generateTypescript(this.parsedSource);
+        }
     }
+}
+
+
+const isDatasignText = (
+    value: string,
+) => {
+    return /data \w+ {/g.test(value);
 }
 
 
