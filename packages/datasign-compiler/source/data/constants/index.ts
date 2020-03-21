18f4272c2b1c0fed9ef074f207d32a4ce65a09ac
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-03-21 23:07:04 +0200

    datasign-compiler > dev: line types

--- packages/datasign-compiler/source/data/constants/index.ts
+++ packages/datasign-compiler/source/data/constants/index.ts
@@ -3,6 +3,13 @@ import {
     TargetGraphQL,
     TargetProtocolBuffers,
     Targets,
+
+    TypeEntityAnnotation,
+    TypeFieldAnnotation,
+    TypeDataStart,
+    TypeDataEnd,
+    TypeDataField,
+    TypeEmptyLine,
 } from '../interfaces';
 
 
@@ -15,4 +22,21 @@ export const targets: Targets = {
     graphql: targetGraphQL,
     protobuf: targetProtocolBuffers,
     typescript: targetTypescript,
+};
+
+
+export const typeEntityAnnotation: TypeEntityAnnotation = 'ENTITY_ANNOTATION';
+export const typeFieldAnnotation: TypeFieldAnnotation = 'FIELD_ANNOTATION';
+export const typeDataStart: TypeDataStart = 'DATA_START';
+export const typeDataEnd: TypeDataEnd = 'DATA_END';
+export const typeDataField: TypeDataField = 'DATA_FIELD';
+export const typeEmptyLine: TypeEmptyLine = 'EMPTY_LINE';
+
+export const lineTypes = {
+    entityAnnotation: typeEntityAnnotation,
+    fieldAnnotation: typeFieldAnnotation,
+    dataStart: typeDataStart,
+    dataEnd: typeDataEnd,
+    dataField: typeDataField,
+    emptyLine: typeEmptyLine,
 };
