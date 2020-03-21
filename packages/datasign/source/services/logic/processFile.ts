0c65d667afc4bb64fb30be600b7f65ee25ca05ca
Jan Cavel <caveljan@gmail.com>
2020-03-21 18:49:16 +0200

    datasign > dev: logic > processFile

--- packages/datasign/source/services/logic/processFile.ts
+++ packages/datasign/source/services/logic/processFile.ts
@@ -6,44 +6,61 @@ import {
 
 import DatasignCompiler from '@plurid/datasign-compiler';
 
+import {
+    ProcessData,
+} from '../../data/interfaces';
 
+
+
+const writeFiles = async (
+    contents: any,
+    targets: any,
+) => {
+
+}
 
 const handleFile = async (
     filepath: string,
+    data: ProcessData,
 ) => {
-    // console.log('filepath', filepath);
-    // console.log('targets', targets);
-    // console.log('output', output);
-    // console.log('resolve', resolve);
+    const {
+        output,
+        resolve,
+        targets,
+    } = data;
+
+    console.log('filepath', filepath);
+    console.log('targets', targets);
+    console.log('output', output);
+    console.log('resolve', resolve);
 
    // check if file is a .datasign file
-   const fileData = await fs.readFile(filepath, 'utf-8');
+
+   const source = await fs.readFile(filepath, 'utf-8');
 
    const compilerData = {
-       source: fileData,
-       targets: [],
+       source,
+       targets,
        options: {
            comments: false,
        },
-   }
-
+   };
    const compiler = new DatasignCompiler(compilerData);
+   const contents = compiler.compile();
 
-   const result = compiler.compile();
-
-   // write files
+   await writeFiles(contents, targets);
 }
 
 
 const processFile = async (
     file: string,
-    data: any,
+    data: ProcessData,
 ) => {
     const filepath = path.join(__dirname, file);
     const statistics = statSync(filepath);
 
     if (!statistics.isDirectory()) {
-        await handleFile(filepath);
+        await handleFile(filepath, data);
     } else {
         // read files from dir
         // loop over files
