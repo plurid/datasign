0af257b58f9e34eabc86ddd6fb5f666a6cb34dc2
Jan Cavel <caveljan@gmail.com>
2020-03-23 23:02:12 +0200

    datasign > dev: cli, utilities

--- packages/datasign/source/services/utilities/index.ts
+++ packages/datasign/source/services/utilities/index.ts
@@ -3,3 +3,27 @@ export const separateList = (
 ) => {
     return value.split(',');
 }
+
+
+export const stringToBoolean = (
+    s: string,
+    valueDefault: boolean = false,
+): boolean => {
+    switch(s.toLowerCase()) {
+        case "true":
+        case "1":
+        case "on":
+        case "yes":
+        case "y":
+            return true;
+
+        case "false":
+        case "0":
+        case "off":
+        case "no":
+        case "n":
+            return false;
+    }
+
+    return valueDefault;
+}
